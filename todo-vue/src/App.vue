57761303a8ab5f5447d7839bf8554bd9dad806b9
AndressaSRodrigues <srandressa2@gmail.com>
2024-01-23 17:43:38 -0300

    Added TodoList component

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <img src="./assets/taskhublogo.png" class="logo" alt="TaskHub Logo" width="auto" />
+    <TodoList title="Organize your daily tasks" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+* {
+  background-color: #FFFFFF;
+  width: 60vw;
+  height: 80vh;
+  border: none;
+  border-radius: 2em;
+}
+
+.container {
+  margin: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  width: 30vw;
+  height: auto;
+  padding: 2em 0 0 0;
+  margin-top: 2em;
 }
 </style>
